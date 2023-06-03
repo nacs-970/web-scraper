7ec1349365159f870a784a30892ba0195325607c
nacs-970 <atitape6958@gmail.com>
2023-06-03 22:32:55 +0700

    06/03/23-10:32:55 PM : add to repo

---
+++ safebooru_scraper.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import re
+import os
+
+spath = 'out'
+
+# create folder
+if not os.path.exists(spath):
+    os.mkdir(spath)
+
+tags = '1girl'
+burl = f'https://safebooru.org/index.php?page=post&s=list&tags={tags}+&pid='
+bs = requests.get(burl).text
+soup = BeautifulSoup(bs,'html.parser')
+
+# Page 40 / 40 = 2
+# Start page 1 = 0 page 2 = 40
+
+maxpage = 80
+#maxpage = int(str(soup.find(['a'],alt = 'last page').get('href')).split('=')[4])//40
+page = 0
+
+print(f'Download {(maxpage//40)+1} pages ...')
+
+while page <= maxpage:
+    
+    aurl = []
+
+    burl = f'https://safebooru.org/index.php?page=post&s=list&tags={tags}+&pid={page}'
+    bs = requests.get(burl).text
+    soup = BeautifulSoup(bs,'html.parser')
+    urls = soup.find_all(['a'],id = re.compile('^p[0-9]+'))
+    # urls = soup.find_all(['a'],href = re.compile('^index\.php\?page\=post\&s\=view\&id=+'))
+
+    for url in urls:
+
+        # iurl = url.get('id').split('p')[1]
+        # aurl.append(f'https://safebooru.org/index.php?page=post&s=view&id={iurl}')
+
+        iurl = url.get('href')
+        aurl.append(f'https://safebooru.org/{iurl}')
+
+    for url in aurl:
+        ibs = requests.get(url).text
+        isoup = BeautifulSoup(ibs,'html.parser')
+        img = isoup.find(['a'],string = "Original image",href = re.compile('^https://safebooru.org//images/+')).get('href')
+        
+        id = url.split('=')[-1] # get image id
+        sid = img.split('/')[-1].split('.')[0][:5] # Get 5 first char
+        fname = f"{id}-{sid}.{img.split('.')[2]}"
+         
+        print(f'Download {fname} from {img} to file')
+        file = os.path.join(spath,fname)
+        print(file)
+        
+        imgu = requests.get(img).content
+        with open(file, 'wb') as f: 
+            f.write(imgu)
+            f.flush()
+            time.sleep(0.5)
+
+    page += 40
---
+++ wallhaven_scraper.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import re
+import os
+
+spath = 'out'
+
+# create folder
+if not os.path.exists(spath):
+    os.mkdir(spath)
+
+burl = 'https://wallhaven.cc/search?categories=110&purity=100&topRange=1M&sorting=toplist&order=desc&ai_art_filter=1&page='
+
+# NSFW uncomment requests.get(x,headers=head)
+#burl = 'https://wallhaven.cc/search?categories=110&purity=001&topRange=1M&sorting=toplist&order=desc&ai_art_filter=1&page='
+
+# cookie login > f12 > network > headers
+cookie = 'cf_clearance=zxczeWqA5yvZbseTZJJKvT1UXAKhFak9gnQqux1QTWU-1683049107-0-250; remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d=eyJpdiI6IktISTRWVDlsemVlZ0JQRDRXRVZqeVE9PSIsInZhbHVlIjoiWGlpc1J4VnNEYzhubEVEeUk1THo1MUVvZ2hEMWwwVWgzNHpvRnk5V2M2Y3F4bTBKU0FzMVdPZEdpYmN3d1phSW5ZZ213S05CRkU0aytIQm14SzZwK1RUaUxOaENhbkRsMG5ZNnFuVHVNU3NHMnhvWCttZFFXbmpRdWZIQU9yU2t6YXlpM0xjWE54Y3JWckNGUHFKcUFpZkFWUlF4V3BEZmVXaUdaeXY4UlBRTHJFc1ZKRDZRMlZ0QUtHVUhIaGdJIiwibWFjIjoiODYwOGQ4ZGRlMzJjNDI3MWE1ZGFlNmEzODEzYjM2NzllNzc4MDQ1YzhkOGQwODdmMGVkYTc1OWYyMTg2YjRiOCJ9; XSRF-TOKEN=eyJpdiI6ImVpNlVmanN3XC91M2JtNXpCRnRKc2N3PT0iLCJ2YWx1ZSI6IndpODU0a28rZ1MrUXZrY1BSU3JlMkEzWFM2QzN3bG9JbFAwcGxTYmp0RkhkZ3VFMzhTSGd2VXZCY0YyRHdEZzYiLCJtYWMiOiJhY2EyMTllN2E1NDgyYTBiZDdiNDZlNTNjMGY2OGY2Y2FlODMzNjk4MjA3YjYxMWVhOTEzZWQ2N2Q2ZTc0NGQ4In0%3D; wallhaven_session=eyJpdiI6ImJScXpQQnByMUpoZGFsZEtOZFkzS1E9PSIsInZhbHVlIjoiSVhFUndsSjBPNFlNZFUzV2ZIUDhQRWhRbXB2bEpLV0VYNnRIXC9TdnNCRGFRb2ZRTWM0RFM1MzJMQ2ZOYkZ0UkgiLCJtYWMiOiI5ODhjNGQ1ZmY1YmM0ZjQzZjQ5YmYyZmIxMDZiMTkwYzE2NGNjMmQ2ZjM4NmFhYTIxZGM4MGYzNTUyZDg4YmNmIn0%3D'
+head = {'Cookie':cookie,'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+tsite = f'{burl}2'
+
+res = requests.get(tsite)
+#res = requests.get(tsite,headers=head)
+
+bs = res.text
+soup = BeautifulSoup(bs,'xml')
+fpage = soup.find_all(class_="thumb-listing-page-header")[-1].find('h2')
+
+maxpage = 3
+#maxpage = int(str(fpage).split('/')[2].strip().split('<')[0]) # all page
+page = 1
+
+# thumb > img
+print(f'Download {maxpage} pages ...')
+while page <= maxpage:
+    site = f'{burl}{page}'
+    
+    res = requests.get(site)
+    #res = requests.get(site,headers=head)
+    
+    bs = res.text
+    soup = BeautifulSoup(bs,'xml')
+    
+    # Grab thumbnail url
+    walls = soup.find_all('a',class_='preview')
+    uwall = []
+    
+    # Add url to list
+    for wall in walls:
+        uwall.append(wall.get('href'))
+    
+    # Download image
+    for url in uwall:
+        
+        res2 = requests.get(url)
+        #res2 = requests.get(url,headers=head)
+        
+        bs2 = res2.text
+        soup2 = BeautifulSoup(bs2,'xml')
+        
+        img = soup2.find_all('img')[-1].get('src')
+        #img = soup2.select('#wallpaper') # Select by id
+        
+        dimg = requests.get(img).content
+        #dimg = requests.get(img,headers=head).content
+        
+        file = os.path.join(spath,img.split('/')[-1])
+        print(f'Download: {img.split("/")[-1]} from {url} to {file}')
+        with open(file, 'wb') as f: 
+         f.write(dimg)
+         f.flush()
+         time.sleep(0.5)
+    page += 1
